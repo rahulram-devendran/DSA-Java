9f765f1a04a6e8380d8f6092d8b1cfee05c53d84
Rahulram240 <devendranrahulram@gmail.com>
2026-07-11 12:32:44 +0530

    Initial Commit for DSA with Two Sum[Two Pointer]

---
+++ src/Two_Sum/Two_Pointer/Two_Sum_1.java
@@ -0,0 +1,35 @@
+package Two_Sum.Two_Pointer;
+
+import java.util.*;
+
+public class Two_Sum_1 {
+
+	public static void main(String[] a) {
+		int n = (int) (Math.random() * 10) + 1;
+		int target = (int) (Math.random() * 10) + 1;
+		System.out.println("The Length of the Array is = " + n);
+		System.out.println("The Target element is " + target);
+		int arr[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = (int) (Math.random() * 10) + 1;
+		}
+		System.out.print("The elements of the Array were = ");
+		for (int i : arr) {
+			System.out.print(i + " ");
+		}
+		System.out.println("\nThe Indexes of the Array that Leads to this Sum is " + twoSum(arr, target));
+	}
+
+	private static String twoSum(int[] arr, int target) {
+		StringBuilder sb = new StringBuilder();
+		Map<Integer, Integer> hm = new HashMap<>();
+		for (int i = 0; i < arr.length; i++) {
+			int exp = target - arr[i];
+			if (hm.containsKey(exp)) {
+				return sb.append(hm.get(exp)).append(" and ").append(i).toString();
+			}
+			hm.put(arr[i], i);
+		}
+		return sb.append("not found").toString();
+	}
+}
